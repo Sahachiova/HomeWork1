bbf0ffa7aa391e65b0b103a17bd45ec251d03038
Svetlana Sahachiova <sahachiova@mail.ru>
2022-06-16 11:08:49 +0300

    Решила половину задачи

--- HomeWork.java
+++ HomeWork.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class HomeWork {
+    public static void main(String[] args) {
+        ArrayList<Integer>numbers=new ArrayList<>();
+        numbers.add(18);
+        numbers.add(3);
+        numbers.add(7);
+        numbers.add(14);
+        numbers.add(142);
+        System.out.println(numbers);
+    }
 }
