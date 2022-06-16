c46d7d241b824969319d236e81e4cfb6e8bcaefd
Svetlana Sahachiova <sahachiova@mail.ru>
2022-06-16 11:13:33 +0300

    Решила задачу и нашла минимально число

--- HomeWork.java
+++ HomeWork.java
@@ -2,12 +2,23 @@ import java.util.ArrayList;
 
 public class HomeWork {
     public static void main(String[] args) {
-        ArrayList<Integer>numbers=new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
         numbers.add(18);
         numbers.add(3);
-        numbers.add(7);
+        numbers.add(-7);
         numbers.add(14);
         numbers.add(142);
-        System.out.println(numbers);
+        findMin(numbers);
+    }
+
+    static int findMin(ArrayList<Integer> numbers) {
+        int sum = 0;
+        for (Integer number : numbers) {
+            if (number < sum) {
+                sum = number;
+                System.out.println("Минимальное число: " + sum);
+            }
+        }
+        return sum;
     }
 }
